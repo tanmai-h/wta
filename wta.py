4eec70dff3130659a7ce3086bf12e60ba531c9aa
Tanmai <vvijaytanmai@gmail.com>
2019-10-23 19:35:09 +0530

    inital files

---
+++ wta.py
@@ -0,0 +1,99 @@
+from keras.preprocessing import image
+from keras.models import Model, Sequential
+from keras.layers import Activation, Dense, GlobalAveragePooling2D, BatchNormalization, Dropout, Conv2D, Conv2DTranspose, AveragePooling2D, MaxPooling2D, UpSampling2D, Input, Reshape
+from keras import backend as K
+from keras.optimizers import Nadam, Adam, SGD
+from keras.metrics import categorical_accuracy, binary_accuracy
+#from keras_contrib.losses import jaccard
+import tensorflow as tf
+import numpy as np
+
+def jaccard_distance(y_true, y_pred, smooth=100):
+    intersection = K.sum(K.abs(y_true * y_pred), axis=-1)
+    sum_ = K.sum(K.square(y_true), axis = -1) + K.sum(K.square(y_pred), axis=-1)
+    jac = (intersection + smooth) / (sum_ - intersection + smooth)
+    return (1 - jac)
+
+def iou(y_true, y_pred, smooth = 100):
+    intersection = K.sum(K.abs(y_true * y_pred), axis=-1)
+    sum_ = K.sum(K.square(y_true), axis = -1) + K.sum(K.square(y_pred), axis=-1)
+    jac = (intersection + smooth) / (sum_ - intersection + smooth)
+    return jac
+
+img_input = Input(shape= (192, 256, 3))
+x = Conv2D(16, (3, 3), padding='same', name='conv1')(img_input)
+x = BatchNormalization(name='bn1')(x)
+x = Activation('relu')(x)
+x = Conv2D(32, (3, 3), padding='same', name='conv2')(x)
+x = BatchNormalization(name='bn2')(x)
+x = Activation('relu')(x)
+x = MaxPooling2D()(x)
+x = Conv2D(64, (3, 3), padding='same', name='conv3')(x)
+x = BatchNormalization(name='bn3')(x)
+x = Activation('relu')(x)
+x = Conv2D(64, (3, 3), padding='same', name='conv4')(x)
+x = BatchNormalization(name='bn4')(x)
+x = Activation('relu')(x)
+x = MaxPooling2D()(x)
+x = Conv2D(128, (3, 3), padding='same', name='conv5')(x)
+x = BatchNormalization(name='bn5')(x)
+x = Activation('relu')(x)
+x = Conv2D(128, (4, 4), padding='same', name='conv6')(x)
+x = BatchNormalization(name='bn6')(x)
+x = Activation('relu')(x)
+x = MaxPooling2D()(x)
+x = Conv2D(256, (3, 3), padding='same', name='conv7')(x)
+x = BatchNormalization(name='bn7')(x)
+x = Dropout(0.5)(x)
+x = Activation('relu')(x)
+x = Conv2D(256, (3, 3), padding='same', name='conv8')(x)
+x = BatchNormalization(name='bn8')(x)
+x = Activation('relu')(x)
+x = MaxPooling2D()(x)
+x = Conv2D(512, (3, 3), padding='same', name='conv9')(x)
+x = BatchNormalization(name='bn9')(x)
+x = Activation('relu')(x)
+x = Dense(1024, activation = 'relu', name='fc1')(x)
+x = Dense(1024, activation = 'relu', name='fc2')(x)
+
+# Deconvolution Layers (BatchNorm after non-linear activation)
+
+x = Conv2DTranspose(256, (3, 3), padding='same', name='deconv1')(x)
+x = BatchNormalization(name='bn19')(x)
+x = Activation('relu')(x)
+x = UpSampling2D()(x)
+x = Conv2DTranspose(256, (3, 3), padding='same', name='deconv2')(x)
+x = BatchNormalization(name='bn12')(x)
+x = Activation('relu')(x)
+x = Conv2DTranspose(128, (3, 3), padding='same', name='deconv3')(x)
+x = BatchNormalization(name='bn13')(x)
+x = Activation('relu')(x)
+x = UpSampling2D()(x)
+x = Conv2DTranspose(128, (4, 4), padding='same', name='deconv4')(x)
+x = BatchNormalization(name='bn14')(x)
+x = Activation('relu')(x)
+x = Conv2DTranspose(128, (3, 3), padding='same', name='deconv5')(x)
+x = BatchNormalization(name='bn15')(x)
+x = Activation('relu')(x)
+x = UpSampling2D()(x)
+x = Conv2DTranspose(64, (3, 3), padding='same', name='deconv6')(x)
+x = BatchNormalization(name='bn16')(x)
+x = Activation('relu')(x)
+x = Conv2DTranspose(32, (3, 3), padding='same', name='deconv7')(x)
+x = BatchNormalization(name='bn20')(x)
+x = Activation('relu')(x)
+x = UpSampling2D()(x)
+x = Conv2DTranspose(16, (3, 3), padding='same', name='deconv8')(x)
+x = BatchNormalization(name='bn17')(x)
+x = Dropout(0.5)(x)
+x = Activation('relu')(x)
+x = Conv2DTranspose(1, (3, 3), padding='same', name='deconv9')(x)
+x = BatchNormalization(name='bn18')(x)
+x = Activation('sigmoid')(x)
+pred = Reshape((192,256))(x)
+
+model = Model(inputs=img_input, outputs=pred)
+
+model.compile(optimizer= Adam(lr = 0.003), loss= [jaccard_distance], metrics=[iou])
+model.load_weights("model.h5")
+
